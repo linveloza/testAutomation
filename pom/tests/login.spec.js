b582bb5ba95b596795f744237fe0fdd4b69a2f29
JuanPulido90 <125696076+JuanPulido90@users.noreply.github.com>
2024-01-19 09:42:32 -0500

    fix

--- pom/tests/login.spec.js
+++ pom/tests/login.spec.js
@@ -2,13 +2,19 @@
 import { test, expect } from '@playwright/test';
 //Se importa lo que creamos en la pagina login.spec.js
 const { LoginPage } = require('../pages/login-page');
+//Vamos a importar la libreria dotenv para que funcione el .env
+import dotenv from 'dotenv'
+dotenv.config();
 
 test('User must be logged in successfully', async ({ page }) => {
-  await page.goto('https://www.saucedemo.com/');
+  //Esto lo cambiaremos trayendo nombrando las variables del .env 
+  //await page.goto('https://www.saucedemo.com/');
+  //quedaria de esta manera
+  await page.goto(process.env.SAUCEDEMOURL);
   //Esto debemos agregarlo
   const loginPage = new LoginPage(page);
   //Aca vamos a llamar el user y el pass de la siguiente manera
-  await loginPage.submitLoginForm('standard_user','secret_sauce')
+  await loginPage.submitLoginForm(process.env.SAUCEDEMOUSER,process.env.SAUCEDEMOPASS);
   //Esta parte del codigo la vamos a arreglar en login.spec.js
   /*await page.locator('[data-test="username"]').fill('standard_user');
   await page.locator('[data-test="password"]').fill('secret_sauce');
